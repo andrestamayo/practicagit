90130c2386b468977078be799a5785e001a712bd
andres <andybmsek@gmail.com>
2015-03-10 17:44:03 +0100

    modificaciones alumno 1

--- GitAux.java
+++ GitAux.java
@@ -1,29 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package prgitejemplo;
-
-/**
- *
- * @author bertoa
- */
 public class GitAux {
-    
-    public void metodoAlumno1(){
-        System.out.println("metodo 1");
-//estoy modificando cosas
-    }
-    
-    public void metodoAlumnoa2(){
-        System.out.println("metodo 2");
-    }
-    
-    public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
-    }
-public void testGitAux(){
- // metodo rama testing
-System.out.println("Metodo de testing");
+
+ public void metodoAlumno1(){
+ System.out.println("metodo 1");
+ System.out.println("modificado por alumno 1"); // alumno 1
  }
-}
+
+ public void metodoAlumno2(){
+ System.out.println("metodo 2");
+ }
+
+ public void metodoComunitario(){
+ // Comentario incluido para pruebas
+ System.out.println("Aqui escribimos todos");
+// modificaciones alumno 1
+System.out.println("Alumno 1 inserta esta sentencia");
+ }
